1f4fc27db2f02728cfc1808f3c7207fd948ae493
Minhaz Chowdhury <63835906+Minhaz2019336040@users.noreply.github.com>
2021-10-24 10:49:02 +0600

    Constructor in Python

---
+++ Constructor in Python/constructor.py
@@ -0,0 +1,31 @@
+# class Mobile:
+#     def __init__(self):
+#         print("Mobile Constructor Called")
+
+# realme = Mobile()
+
+
+# class Mobile:
+#     def __init__(self):
+#         self.model = "Realme X"
+    
+#     def show_model(self):
+#         print(f"Model: {self.model}")
+
+# realme = Mobile()
+# realme.show_model()
+
+class Mobile:
+    def __init__(self, m, v=80):
+        self.model = m
+        self.volumn = v
+    
+    def show_model(self, p):
+        price = p
+        print(f"Model: {self.model}, Volumn: {self.volumn}, Price: {price}")
+
+realme = Mobile("Realme X", 50)
+realme.show_model(25000)
+
+redmi = Mobile("Redmi XL")
+redmi.show_model(25000)
